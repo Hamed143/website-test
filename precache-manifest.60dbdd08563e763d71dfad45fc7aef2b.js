2a178171ac10c4877ca536f6ebc4aa38d2e89f8e
hamed143 <hamed_hossainy@yahoo.com>
2019-11-04 15:17:48 +0100

    Updates

---
+++ precache-manifest.60dbdd08563e763d71dfad45fc7aef2b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bf1c3d892c7b7b62b928a2118cca30f",
+    "url": "/website-test/index.html"
+  },
+  {
+    "revision": "cc84d3811aa0e64c0005",
+    "url": "/website-test/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "359ace3032c95558600d",
+    "url": "/website-test/static/js/2.143d97ab.chunk.js"
+  },
+  {
+    "revision": "cc84d3811aa0e64c0005",
+    "url": "/website-test/static/js/main.747da14c.chunk.js"
+  },
+  {
+    "revision": "6b6d24d5512476cbc7cb",
+    "url": "/website-test/static/js/runtime-main.2909481d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/website-test/static/media/logo.25bf045c.svg"
+  }
+]);
